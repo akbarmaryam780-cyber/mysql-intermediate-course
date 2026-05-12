45c2b3da9e0beae8c500a17c546c6a25c11b10f8
akbarmaryam780-cyber <akbarmaryam780@gmail.com>
2026-05-12 11:14:07 +0000

    Add SQL exercises for CASE statements and calculated columns

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,17 @@
-/*
-Example lesson stub
-*/
 
 SELECT
-	*
+    ps.PatientId
+    ,ps.AdmittedDate
+    ,ps.DischargeDate
+    ,ps.Hospital
+    ,ps.Ward
+    ,DATEADD(DAY, -14, ps.AdmittedDate) AS ReminderDate
+    ,DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) + 1 AS LengthOfStay
 FROM
-	PatientStay ps ;
+    PatientStay AS ps
+WHERE ps.Hospital IN ('Oxleas', 'PRUH')
+    AND ps.AdmittedDate BETWEEN '2024-02-01'  AND  '2024-03-01'
+    AND ps.Ward LIKE  'D%'
+ORDER BY
+  ps.AdmittedDate DESC
+  ,ps.PatientId DESC;
